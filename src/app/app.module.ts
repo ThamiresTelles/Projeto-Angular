bb8b2efa88073d3830e5319540d1ab1d6dc19795
Thamires Telles <thamirestelles12@gmail.com>
2021-05-14 02:42:59 -0300

    Criação do card principal

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { CardPrincipalComponent } from './card-principal/card-principal.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CardPrincipalComponent
   ],
   imports: [
     BrowserModule
