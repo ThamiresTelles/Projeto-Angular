ac66bdea050034b41e91473a43327fc61950802c
Thamires Telles <thamirestelles12@gmail.com>
2021-05-14 03:12:20 -0300

    Adição do ngIf no card principal

--- src/app/card-principal/card-principal.component.ts
+++ src/app/card-principal/card-principal.component.ts
@@ -7,6 +7,9 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CardPrincipalComponent implements OnInit {
 
+
+  mostrarFonte = false; 
+
   constructor() { }
 
   ngOnInit(): void {
